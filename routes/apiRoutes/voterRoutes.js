5f99d0fddfd1ec29e59f0f9faed812149d44f5c3
detrasmental <detra.gordon76@gmail.com>
2021-11-04 12:22:59 -0400

    added voter routes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -38,56 +38,55 @@ router.get('/voter/:id', (req, res) => {
 
 // Create a voter
 router.post('/voter', ({ body }, res) => {
-  // Data validation
-  const errors = inputCheck(body, 'first_name', 'last_name', 'email');
-  if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-  }
-
-  const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
-  const params = [body.first_name, body.last_name, body.email];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
+// Data validation
+const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+if (errors) {
+  res.status(400).json({ error: errors });
+  return;
+}
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
     });
   });
-});
 
 // Update a voter's email
 router.put('/voter/:id', (req, res) => {
-  // Data validation
-  const errors = inputCheck(req.body, 'email');
-  if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-  }
-
-  const sql = `UPDATE voters SET email = ? WHERE id = ?`;
-  const params = [req.body.email, req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: 'Voter not found'
-      });
-    } else {
-      res.json({
-        message: 'success',
-        data: req.body,
-        changes: result.affectedRows
-      });
+    // Data validation
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
     }
+  
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Voter not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
   });
-});
 
 // Delete a voter
 router.delete('/voter/:id', (req, res) => {
